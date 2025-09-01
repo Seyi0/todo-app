1ae80db1e6d3202bd3fecc7ab673032d9e105c90
Seyi0 <makanjuolaseyi560@gmail.com>
2025-09-01 11:20:33 +0100

    :done:

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,14 +3,60 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import db from '../db.js'
 
-const router = express.Router ()
+const router = express.Router()
 
-router.post('/register', (req,res)=>{
+// Register a new user endpoing /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    // save the username and an irreversibly encrypted password
+
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
+
+    // save the new user and hashed password to the db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        // now that we have a user, I want to add their first todo for them
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+})
+
+router.post('/login', (req, res) => {
+   
+    const { username, password } = req.body
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        // if the password does not match, return out of the function
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
+        console.log(user)
+
+        // create a token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 
 })
 
-router.post('/login', (req,res)=>{
-    
-})
 
 export default router
